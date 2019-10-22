d0d2fd7bd0b4f2ec73713fd79e699130366c010b
hrk794 <hrk794@gmail.com>
2019-10-22 21:05:57 +0530

    Exercise 1

---
+++ ex1.asm
@@ -0,0 +1,12 @@
+;  x86 Assembly exercise 1
+
+
+global _start  			; global denotes the entry point of the program
+_start: 			; Everything that starts with an underscore is a label
+	mov eax, 1		; moves 1 to EAX register
+	mov ebx, 42		; moves 42 to EBX register
+	int 0x80		; Causes an interupt to occur, 0x80 is for system calls. 
+				; Type of system call is determined by the value in EAX, 1 means exit
+				; and the exit status is taken from EBX register
+				; This can be verified with the 'echo $?' command after 
+				; executing the executable file
